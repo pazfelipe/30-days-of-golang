5679efd0c95e0cd4ca721de92bd37330446f28d9
Felipe Paz <xxfelipepaz@gmail.com>
2023-03-11 16:46:43 -0300

    feat: add switch statement

--- day-2_control_structures/main.go
+++ day-2_control_structures/main.go
@@ -13,4 +13,26 @@ func main() {
 	} else {
 		fmt.Println("You are a child")
 	}
+
+	age = 35
+
+	switch {
+	case age < 18:
+		fmt.Println("You are a child")
+	case age >= 18 && age < elder_age:
+		fmt.Println("You are an adult")
+	default:
+		fmt.Println("You are an elder")
+	}
+
+	switch age {
+	case 17:
+		fmt.Println("You are 17")
+	case 18:
+		fmt.Println("You are 18")
+	case 35:
+		fmt.Println("You are 35")
+	default:
+		fmt.Println("You are not 17, 18 or 35")
+	}
 }
