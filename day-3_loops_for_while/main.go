1a992f7cd657b7d89a319993768de1cf0cf74c9a
Felipe Paz <xxfelipepaz@gmail.com>
2023-03-11 15:59:32 -0300

    feat: create day 3

---
+++ day-3_loops_for_while/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	// for loop
+	for i := 0; i < 10; i++ {
+		println(i)
+	}
+
+	// while loop
+	var i int = 0
+	for i < 10 {
+		println(i)
+		i++
+	}
+
+	// infinite loop / while
+	for {
+		println("infinite loop")
+	}
+}
